5ceaf537edb7a38b870d4be502a3af032e10a560
Mirkocapi78 <mirkocapi878@gmail.com>
2025-05-05 11:43:28 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+const parser = require('./parser-lathe');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'renderer.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+ipcMain.handle('select-file', async () => {
+  const { canceled, filePaths } = await dialog.showOpenDialog({
+    filters: [{ name: 'ISO CNC Files', extensions: ['txt', 'iso', 'm', 'cnc'] }],
+    properties: ['openFile']
+  });
+  if (canceled) return null;
+  return filePaths[0];
+});
+
+ipcMain.handle('calculate-time', async (event, filepath) => {
+  const fs = require('fs');
+  const content = fs.readFileSync(filepath, 'utf8');
+  const commands = parser.parseISO(content);
+  const seconds = parser.computeLatheTime(commands);
+  return seconds;
+});
