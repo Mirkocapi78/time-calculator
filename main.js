e8b0b7efe693e26d4bd62a345fbd9cfbcfdcc501
Mirkocapi78 <mirkocapi878@gmail.com>
2025-05-05 12:44:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'renderer.js'),
+-     preload: path.join(__dirname, 'renderer.js'),
++     preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
       contextIsolation: true
     }
