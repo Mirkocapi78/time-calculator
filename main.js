b11dc65248bcd658fb220cf46dc642d6e306015c
Mirkocapi78 <mirkocapi878@gmail.com>
2025-05-06 10:31:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// main.js  – processo principale Electron
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
 const parser = require('./parser-lathe');
@@ -7,7 +8,7 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, 'preload.js'), // ← carica il bridge
       nodeIntegration: false,
       contextIsolation: true
     }
@@ -17,20 +18,23 @@ function createWindow() {
 
 app.whenReady().then(createWindow);
 
+/* ---- IPC: dialogo di apertura file ---- */
 ipcMain.handle('select-file', async () => {
   const { canceled, filePaths } = await dialog.showOpenDialog({
-    filters: [{ name: 'ISO CNC Files', extensions: ['txt', 'iso', 'm', 'cnc'] }],
-    properties: ['openFile']
+    properties: ['openFile'],
+    filters: [
+      { name: 'ISO CNC Files', extensions: ['txt', 'iso', 'm', 'cnc'] }
+    ]
   });
   if (canceled) return null;
   return filePaths[0];
 });
 
+/* ---- IPC: calcolo tempo ---- */
 ipcMain.handle('calculate-time', async (event, { path, rpmMax }) => {
   const fs = require('fs');
   const content = fs.readFileSync(path, 'utf8');
   const cmds = parser.parseISO(content);
-  const seconds = parser.computeLatheTime(cmds, rpmMax);
-  return seconds;
+  const sec  = parser.computeLatheTime(cmds, rpmMax);
+  return sec;
 });
-
