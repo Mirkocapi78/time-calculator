106abf119d8a529612872de13faeb06292066de9
Mirkocapi78 <mirkocapi878@gmail.com>
2025-05-06 10:31:40 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,11 +1,10 @@
-// preload.js
+// preload.js  – bridge sicuro renderer ↔ main
 const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('api', {
-  selectFile: () => ipcRenderer.invoke('select-file'),
-  calcTime: (path) => ipcRenderer.invoke('calculate-time', path)
-});
-contextBridge.exposeInMainWorld('api', {
-  selectFile: () => ipcRenderer.invoke('select-file'),
-  calcTime:  (path, rpmMax) => ipcRenderer.invoke('calculate-time', { path, rpmMax })
-});
 
+contextBridge.exposeInMainWorld('api', {
+  selectFile: () => ipcRenderer.invoke('select-file'),
+
+  // accetta path + limite giri (rpmMax)
+  calcTime:  (path, rpmMax) =>
+    ipcRenderer.invoke('calculate-time', { path, rpmMax })
+});
