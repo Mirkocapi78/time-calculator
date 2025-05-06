9ae6c7a4bcd9bb92b0ec66f354d6e15e034662ad
Mirkocapi78 <mirkocapi878@gmail.com>
2025-05-06 09:20:52 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,3 +4,7 @@ contextBridge.exposeInMainWorld('api', {
   selectFile: () => ipcRenderer.invoke('select-file'),
   calcTime: (path) => ipcRenderer.invoke('calculate-time', path)
 });
+contextBridge.exposeInMainWorld('api', {
+  selectFile: () => ipcRenderer.invoke('select-file'),
+  calcTime:  (path, rpmMax) => ipcRenderer.invoke('calculate-time', { path, rpmMax })
+});
