4f0c857f5d6e609547d55732dcc1ed4f40f33c99
Mirkocapi78 <mirkocapi878@gmail.com>
2025-05-14 09:21:29 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,12 +1,8 @@
 // preload.js
 const { contextBridge, ipcRenderer } = require('electron');
-
 contextBridge.exposeInMainWorld('api', {
-  // Apre la finestra di dialogo per selezionare un file
   selectFile: () =>
     ipcRenderer.invoke('select-file'),
-
-  // Calcola il tempo: filePath = percorso, mode = 'lathe'|'mill', rpmMax = numero
-  calculateTime: (filePath, mode, rpmMax) =>
-    ipcRenderer.invoke('calculate-time', { path: filePath, mode, rpmMax })
+  calculateTime: (filePath, rpmMax, mode) =>
+    ipcRenderer.invoke('calculate-time', { path: filePath, rpmMax, mode })
 });
